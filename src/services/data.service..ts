3c113d94d2526173d2bea9cc3ce3d51dda2bc40f
eliwuu <marekwilk@me.com>
2022-08-05 23:29:24 +0200

    Initial data source caching

--- src/services/data.service..ts
+++ src/services/data.service..ts
@@ -1,8 +1,37 @@
+import path from 'path';
+import fs from 'fs/promises';
 import { DataSource } from '../model/db';
 
 export default class DB {
-  public static getGenres(dataSource: DataSource): string[] {
-    return dataSource.genres.map((x) => x.normalize());
+  private readonly filename = 'db.json';
+  private dataSource: DataSource | null = null;
+
+  public async loadDataSource() {
+    const filePath = path.resolve(__dirname, 'src', 'data');
+    const loadFile = await fs.readFile(path.join(filePath, this.filename), {
+      encoding: 'utf-8',
+    });
+
+    const dataSource = JSON.parse(loadFile) as DataSource;
+    this.dataSource = dataSource;
   }
-  public static getMovies(filter: Filter, search: Search): Movie[] {}
+
+  public invalidateCache() {
+    this.dataSource = null;
+  }
+  public getGenres(): string[] {
+    if (this.dataSource === null) {
+      this.dataSource = this.refreshCache();
+    }
+    return this.dataSource!.genres.map((x) => x.normalize());
+  }
+  //   public getMovies(filter: Filter): Movie[] {
+  //     if ()
+  //   }
+
+  private async refreshCache(): DataSource {
+    if (this.dataSource === null) {
+      this.dataSource = await this.loadDataSource();
+    }
+  }
 }
