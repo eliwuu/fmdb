1b258fea74d6f76a324bd812c4895070fd7917ba
eliwuu <marekwilk@me.com>
2022-08-06 19:05:23 +0200

    Updated QuerySelector

--- src/services/filter.service.ts
+++ src/services/filter.service.ts
@@ -1,18 +1,12 @@
-import { FilterQuery } from 'mongoose';
 import { Movie } from '../model/movie';
-
-// export function filter: FilterQuery()
 
 export type Filter = { [P in keyof Movie]?: QuerySelector };
 
 type QuerySelector = {
   $eq?: string;
-  $rng?: { from: Date | number; to: Date | number };
-};
-
-const filter: Filter = {
-  title: { $eq: 'ac' },
-  runtime: { $rng: { from: 10, to: 80 } },
+  $like?: string;
+  $gt?: number;
+  $lt?: number;
 };
 
 const usefilter = (aFitler: Filter, dataSource: Movie[]) => {
