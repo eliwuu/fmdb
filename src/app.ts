4b2a7915b40ae7299b62d93782e1930471e78e4f
Marek Wilk <marek.wilk@me.com>
2022-08-09 21:34:18 +0200

    Start express server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import path from 'path';
 import process from 'process';
+import express from 'express';
+
+import genreController from './controllers/genre.controller';
+import movieController from './controllers/movie.controller';
 
 import { DataService } from './services/data.service';
 import { Logger } from './services/logger.service';
@@ -7,9 +11,28 @@ import { Logger } from './services/logger.service';
 export let dataSource: DataService | null = null;
 
 const init = async () => {
-  const dataSourcePath = path.join(__dirname, 'data', process.env.DS_FILE!);
+  const dataSourcePath = path.join(
+    __dirname,
+    'data',
+    process.env.DS_FILE ?? 'db.json'
+  );
+
   dataSource = new DataService(dataSourcePath, Logger);
-  await dataSource.init();
+  const { status, msg } = await dataSource.init();
+
+  if (status === 'error') {
+    Logger.error('Data source initialization failed: ' + msg);
+    process.exit(1);
+  }
+  console.log('Data source initialized');
 };
 
-init();
+const app = express();
+
+app.use('/api/genres', genreController);
+app.use('/api/movies', movieController);
+
+app.listen(process.env.PORT || 3000, async () => {
+  await init();
+  console.log('Server started on port: ' + (process.env.PORT || 3000));
+});
