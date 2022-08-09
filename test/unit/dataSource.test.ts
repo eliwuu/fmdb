ab64fbf5744688535a9feac8a260776cc0ffbfc6
Marek Wilk <marek.wilk@me.com>
2022-08-09 21:36:21 +0200

    Update path for dev env

--- test/unit/dataSource.test.ts
+++ test/unit/dataSource.test.ts
@@ -3,9 +3,16 @@ import path from 'path';
 import { DataService } from '../../src/services/data.service';
 import { Logger } from '../../src/services/logger.service';
 
-const filePath = path.join(__dirname.replace('test', 'src'), 'data', 'db.json');
+const replaceDirectory =
+  process.env.ENV === 'development' ? 'test/unit' : 'test';
+const filePath = path.join(
+  __dirname.replace(replaceDirectory, 'src'),
+  'data',
+  'db.json'
+);
 
 test('Succesfully initialize data source', async () => {
+  console.log(filePath);
   const dataSource = new DataService(filePath, Logger);
 
   const result = await dataSource.init();
@@ -26,7 +33,7 @@ test('Change to database invalidates cache', async () => {
 
   dataSource.invalidateCache();
 
-  const result = dataSource.getData();
+  const result = await dataSource.getData();
 
-  expect(result).toBe(undefined);
+  expect(result).toBe(null);
 });
