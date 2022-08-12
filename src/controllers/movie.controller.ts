c7fe02976c507310d353b8cc549c9d2bdb0c8c4e
Marek Wilk <marek.wilk@me.com>
2022-08-12 04:28:27 +0200

    Implement add endpoint, refactor get endpoint

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -7,34 +7,27 @@ import MovieService from '../services/movie.service';
 const movieController = Router();
 
 movieController.get('/', async (req, res) => {
-  const { runtime } = req.query;
-  //   const runtime = req.query.runtime;
-  //   const genres = req.params.genres;
-
-  console.log(runtime);
+  const { runtime, genres } = req.query;
 
   const parsedRuntime = runtime ? parseInt(runtime as string) : undefined;
 
   const data = await dataSource?.getData();
 
   if (data === undefined || data === null) {
-    res.status(500).send('Internal server error');
+    return res.status(500).send('Internal server error');
   }
-  if (runtime !== undefined) {
-    const movies = MovieService.get(data!)({
-      runtime: parsedRuntime,
-    });
 
-    console.table(movies);
+  const movies = MovieService.get(data!)({
+    runtime: parsedRuntime,
+    genres: genres as string[],
+  });
 
+  if (genres === undefined) {
     const randomMovie = Math.floor(Math.random() * movies.length);
 
-    res.status(200).json(movies[randomMovie]);
+    return res.status(200).json(movies[randomMovie]);
   }
-
-  //   res
-  //     .status(200)
-  //     .json(data!.movies[Math.floor(Math.random() * data!.movies.length)]);
+  res.status(200).json(movies);
 });
 
 movieController.post('/add', async (req, res) => {
@@ -43,19 +36,39 @@ movieController.post('/add', async (req, res) => {
   const validate = MovieService.validate(movie);
 
   if (validate.error) {
-    res.status(400).json(validate.error);
+    return res.status(400).json(validate.error);
   }
 
   const data = await dataSource?.getData();
 
+  if (data === undefined || data === null) {
+    return res.status(500).send('Internal server error');
+  }
+
+  const checkGenres = MovieService.checkGenres(data!)(movie.genres);
+  const isDuplicate = MovieService.isDuplicate(data!)(movie.title);
+
+  if (!checkGenres) {
+    return res.status(400).json('You can only use genres from the list');
+  }
+
+  if (isDuplicate) {
+    return res.status(400).json('Movie already exists');
+  }
+
   const newMovie = MovieService.add(data!)(movie);
+
   const updateStatus = dataSource?.updateDataSource(newMovie);
   dataSource?.invalidateCache();
   await dataSource?.refreshCache();
 
   if (newMovie instanceof Error) {
-    res.status(500).send(newMovie.message);
+    return res.status(500).send(newMovie.message);
   }
+  if (updateStatus instanceof Error) {
+    return res.status(500).send(updateStatus.message);
+  }
+
   res.status(200).json(newMovie);
 });
 
